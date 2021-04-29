b481a6f52030f29ea9f361faf29126ec4da95368
Peter Gonzalez <p8gonzal@ucsd.edu>
2021-04-28 22:15:15 -0700

    Testing push

--- git_script.sh
+++ git_script.sh
@@ -9,4 +9,14 @@ git status
 read -p "Enter commit message: " message
 git commit -m "$message"
 echo "Changes saved..."
+
+elif [ "$COMMAND" == "push" ]; then
+git add .
+echo "The following files were staged:"
+git status
+read -p "Enter commit message: " message
+git commit -m "$message"
+echo "Changes saved..."
+read -p "Confirm push to remote: [y/n]" approval
+
 fi
